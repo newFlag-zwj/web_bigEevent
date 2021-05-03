4e1f3c035473c36f64b1924e773396655bd478e4
newFlag <newFlag_zwj@163.com>
2021-05-03 20:14:01 +0800

    完成了主页内容的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,20 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://127.0.0.1:3007' + options.url;
+
+
+    // 统一为有权限的接口设置header属性
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    options.complete = function(res) {
+        console.log(res)
+        if (res.responseJSON.status === 1 || res.responseJSON.msg == '身份认证失败') {
+            localStorage.removeItem('token')
+            location.href = 'login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+$(function() {
+
+    var layer = layui.layer;
+    getUserinfo()
+
+
+    // 退出登陆
+    $('#btnLogout').on('click', function() {
+        layer.confirm('确认退出登陆?', { icon: 3, title: '提示' }, function(index) {
+            // 清除本地缓存的token
+            localStorage.removeItem('token')
+            location.href = 'login.html'
+
+            layer.close(index);
+        });
+    })
+
+    // 获取用户的基本信息
+    function getUserinfo() {
+        $.ajax({
+            type: "get",
+            url: "/my/userinfo",
+            // headers: {
+            //     Authorization: localStorage.getItem('token')
+            // },
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败')
+                }
+                renderAvatar(res.data)
+                console.log(res)
+            },
+            // complete: function(res) {
+            //     console.log(res)
+            //     if (res.responseJSON.status === 1 || res.responseJSON.msg == '身份认证失败') {
+            //         localStorage.removeItem('token')
+            //         location.href = 'login.html'
+            //     }
+            // }
+
+        });
+    }
+
+
+    // 渲染用户头像
+    function renderAvatar(user) {
+        // 渲染用户名
+        var name = user.nickname || user.username
+        $('#welcome').html('欢迎&nbsp;&nbsp' + name)
+            // 渲染用户头像
+        if (user.user_pic !== null) {
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+            $('.text-avatar').hide()
+        } else {
+            $('.layui-nav-img').hide()
+            $('.text-avatar').html(name[0].toUpperCase()).show()
+        }
+    }
+
+
+
+})
